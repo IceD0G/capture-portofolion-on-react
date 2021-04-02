5aa489fa8a9f689722cd5114c9f82f81b88a7d4a
IceD0G <34693762+IceD0G@users.noreply.github.com>
2021-04-02 20:55:29 +0300

    Lesson 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import AboutUs from './pages/AboutUs';
+//Impoort Pages
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Film</h1>
+			<AboutUs />
 		</div>
 	);
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+import FaqSection from '../components/FaqSection';
+import ServecesSection from '../components/ServecesSection';
+
+const AboutUs = () => {
+	return (
+		<>
+			<AboutSection />
+			<ServecesSection />
+			<FaqSection />
+		</>
+	);
+};
+
+export default AboutUs;
