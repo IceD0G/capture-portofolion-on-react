95e56edf4d960448d7e32641858dd98e7651a9cf
IceD0G <34693762+IceD0G@users.noreply.github.com>
2021-04-08 18:32:39 +0300

    sda

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
 //Impoort Pages
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+import Nav from './components/Nav';
+import MovieDetail from './pages/MovieDetails';
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='App'>
 			<GlobalStyle />
-			<AboutUs />
+			<Nav />
+			<Switch>
+				<Route path='/' exact>
+					<AboutUs />
+				</Route>
+				<Route path='/work' exact>
+					<OurWork />
+				</Route>
+				<Route path='/work/:id'>
+					<MovieDetail />
+				</Route>
+				<Route path='/contact'>
+					<ContactUs />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
